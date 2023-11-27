aa4463c20fa8f0032c428c434529154f4f72cc96
Tho-gaKo <thomas-gabriel.korak@coop.ch>
2023-11-27 11:29:05 +0100

    mit super.get... die Methode zur Ausgabe von numberOfDoors

--- src/main/java/Vererbung/KlassenUndObjekte/Fahrzeug.java
+++ src/main/java/Vererbung/KlassenUndObjekte/Fahrzeug.java
@@ -2,6 +2,10 @@ package Vererbung.KlassenUndObjekte;
 
 public class Fahrzeug {
 
+    private String carBrand;
+    private String carModel;
+    private int buildYear;
+
     public static void main(String[] args) {
         car porsche = new car();
         porsche.setBuildYear(2016);
@@ -11,11 +15,46 @@ public class Fahrzeug {
 
         car porsche2 = new car();
         porsche2.setBuildYear(1942);
-        porsche2.setCarModel("Maus");
+        porsche2.setCarModel("PzKw VIII Maus");
         porsche2.setNumberOfDoors(0);
         porsche2.setCarBrand("Porsche");
 
+        printFahrzeug(porsche);
 
     }
 
+    @Override
+    public String toString() {
+
+        return carBrand +" "+ carModel +" "+ buildYear;
+    }
+
+    public static void printFahrzeug (car a){
+        System.out.println(a);
+
+    }
+
+    public String getCarBrand() {
+        return carBrand;
+    }
+
+    public void setCarBrand(String carBrand) {
+        this.carBrand = carBrand;
+    }
+
+    public String getCarModel() {
+        return carModel;
+    }
+
+    public void setCarModel(String carModel) {
+        this.carModel = carModel;
+    }
+
+    public int getBuildYear() {
+        return buildYear;
+    }
+
+    public void setBuildYear(int buildYear) {
+        this.buildYear = buildYear;
+    }
 }
--- src/main/java/Vererbung/KlassenUndObjekte/car.java
+++ src/main/java/Vererbung/KlassenUndObjekte/car.java
@@ -1,39 +1,18 @@
 package Vererbung.KlassenUndObjekte;
 
-public class car {
+public class car extends Fahrzeug {
 
-    private String carBrand;
-    private String carModel;
-    private int buildYear;
+
     private int numberOfDoors;
 
 
-    public String getCarBrand() {
-        return carBrand;
-    }
-
-    public void setCarBrand(String carBrand) {
-        this.carBrand = carBrand;
-    }
-
-    public String getCarModel() {
-        return carModel;
-    }
-
-    public void setCarModel(String carModel) {
-        this.carModel = carModel;
-    }
-
-    public int getBuildYear() {
-        return buildYear;
-    }
-
-    public void setBuildYear(int buildYear) {
-        this.buildYear = buildYear;
-    }
-
     public int getNumberOfDoors() {
         return numberOfDoors;
+    }
+    @Override
+    public String toString() {
+
+        return super.getCarBrand() +" "+ super.getCarModel() +" "+ super.getBuildYear() + " " + numberOfDoors;
     }
 
     public void setNumberOfDoors(int numberOfDoors) {
