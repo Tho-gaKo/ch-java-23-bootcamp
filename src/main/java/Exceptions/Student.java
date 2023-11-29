768586b05e8719deb2bffcab2a5caf8305038dfe
Tho-gaKo <thomas-gabriel.korak@coop.ch>
2023-11-29 14:45:18 +0100

    Exceptions initial

---
+++ src/main/java/Exceptions/Student.java
@@ -0,0 +1,13 @@
+package Exceptions;
+
+import lombok.Builder;
+import lombok.With;
+
+@Builder
+@With
+public record Student(
+        String id,
+        String name,
+        String subject
+) {
+}
