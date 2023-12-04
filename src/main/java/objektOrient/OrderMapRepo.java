d49be08948ad9077c3c8e8cd127c72244143fb2e
Tho-gaKo <thomas-gabriel.korak@coop.ch>
2023-12-04 16:34:03 +0100

    Shopservice

---
+++ src/main/java/objektOrient/OrderMapRepo.java
@@ -0,0 +1,20 @@
+package objektOrient;
+
+import java.util.ArrayList;
+
+public class OrderMapRepo implements OrderRepo{
+    @Override
+    public void addOrder(Order order, ArrayList list) {
+    order
+    }
+
+    @Override
+    public void remOrder(Order order) {
+
+    }
+
+    @Override
+    public void getOrder(Order order) {
+
+    }
+}
---
+++ src/main/java/objektOrient/OrderRepo.java
@@ -0,0 +1,11 @@
+package objektOrient;
+
+public interface OrderRepo {
+    public void addOrder(Order order);
+
+    public void remOrder(Order order);
+
+    public void getOrder(Order order);
+
+
+}
